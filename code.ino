33f753772e353e53939ed906477c1b750912d34c
Fajar Julyana <90027748+fajarjulyana@users.noreply.github.com>
2023-04-07 11:20:56 +0700

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,6 +1,6 @@
 // constants won't change. They're used here to set pin numbers:
 const int BUTTON_PIN = 4;       // the number of the pushbutton pin
-const int PinSaya = 2;
+const int PinLED = 2;
 // Variables will change:
 int lastState = LOW;  // the previous state from the input pin
 int currentState;                // the current reading from the input pin
@@ -12,20 +12,20 @@ void setup() {
         // initialize the pushbutton pin as an pull-up input
           // the pull-up input pin will be HIGH when the switch is open and LOW when the switch is closed.
             pinMode(BUTTON_PIN, INPUT_PULLUP);
-            }
+     }
 
-            void loop() {
-              // read the state of the switch/button:
-                currentState = digitalRead(BUTTON_PIN);
+void loop() {
+  // read the state of the switch/button:
+     currentState = digitalRead(BUTTON_PIN);
 
-                  if(lastState == HIGH && currentState == LOW){
-                      Serial.println("The button is pressed");
-                          digitalWrite(PinSaya, HIGH);}
-                            else if(lastState == LOW && currentState == HIGH){
-                                Serial.println("The button is released");
-                                digitalWrite(PinSaya, LOW);
-                                        }
-                                          // save the the last state
+     if(lastState == HIGH && currentState == LOW){
+        Serial.println("The button is pressed");
+        digitalWrite(PinLED, HIGH);
+     }else if(lastState == LOW && currentState == HIGH){
+        Serial.println("The button is released");
+        digitalWrite(PinLED, LOW);
+     }
+ // save the the last state
                                             lastState = currentState;
                                                     
                                                     }
